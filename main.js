dd40aa6248c87963a82d2274ef387850836d7d91
Josefina Dorola <josefina.dorola@gmail.com>
2023-04-08 11:06:11 -0300

    ejercicios

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ parImpar(5);
 
 function esMayorIgual(num1, num2){
         if (num1 === num2){
-            console.log("Los números son igulaes"); 
+            console.log("Los números son iguales"); 
         }
 
         else if (num1 < num2)   {
@@ -40,37 +40,40 @@ function esMultiplo5(numero){
     console.log(`El número ${numero} no es múltiplo de 5`);
 }
 
-esMultiplo5(25);
+esMultiplo5(23);
 
 
 
 //4- Crear una función que reciba un número por parámetro e imprima por consola todos los números desde el 0 hasta llegar a ese número.
 
 
-function ciclo(num1){
-    let num2 = 0;
-    while(num2 < num1){
-        console.log(num2) 
-        num2++;
+function ciclo(cantidad){
+    for(i=0; i < cantidad; i++){
+        console.log(i);
     }
-    
-    }
+}
 
-ciclo(10);
-
-
-
+ciclo(15);
 
 
 //5 - Crear una función que reciba una palabra y un número por parámetro e imprima por consola  esa palabra la cantidad correspondiente al número indicado.
 
+function imprimirPalabra(palabra, cant){
+    for(i=0; i < cant; i++){
+        console.log(palabra);
+}
+}
+
+imprimirPalabra("Messi", 10);
 
 
 
 //6 - Crear una función que reciba un array por parámetro e imprima por consola todos los valores de ese array.
 
-function arrayParametro([array]){
-    console.log(array);
+function arrayParametro(array){
+    for (let i=0; i < array.length; i++){
+    console.log(array[i]);
+    }
 }
 arrayParametro(["Leo", "Fideo", "Julián", "Enzo"]);
 
@@ -80,18 +83,28 @@ arrayParametro(["Leo", "Fideo", "Julián", "Enzo"]);
 
 function miArray(){
     let num = [0,1,2,3,4,5,6,7,8,9];
-    for (let i=0; i < num.length; i++){
-        if (num[5]){
-            continue;
-        }
-        console.log(num[i]);
+      for (let i=0; i < num.length; i++){
+        if (i == 4)
+        continue;{
+            
+       }
+       console.log(num[i]);    
     }
 }
     miArray();
 
+  
 
 
 //8 - Crea una función que reciba un array de números y un número por parámetro e imprima por consola cada número del array multiplicado por el número pasado por parámetro.
 
 
+function multiplicarArray(multiplicar){
+    let array = [2,5,10,15];
+    for (let i=0; i < array.length; i++){
+        console.log(array[i] * multiplicar);
+    }
+}
+
+multiplicarArray(3);
 
