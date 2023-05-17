36d6684332379caaeaa0ff94d7634a1fec04595e
Josefina Dorola <josefinad@hendel-hogar.com.ar>
2023-05-17 16:36:56 -0300

    ejercicio 3

--- index.js
+++ index.js
@@ -1,82 +1,100 @@
-const pizzas = [
-  {
-    id: 1,
-    nombre: "pizza de Muzzarella",
-    precio: 500,
-    ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
-    imagen: "./img/muzzarella.png",
-  },
+    const form = document.getElementById('form');
+    const resultado = document.getElementById('resultado');
 
-  {
-    id: 2,
-    nombre: "pizza de Cebolla",
-    precio: 1500,
-    ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
-    imagen: "./img/cebolla.png",
-  },
+    form.addEventListener('submit', submitHandle);
 
-  {
-    id: 3,
-    nombre: "pizza 4 Quesos",
-    precio: 1380,
-    ingredientes: [
-      "Muzzarella",
-      "Tomate",
-      "Queso Azul",
-      "Parmesano",
-      "Roquefort",
-    ],
-    imagen: "./img/4quesos.png",
-  },
+    function submitHandle(e) {
+      e.preventDefault();
 
-  {
-    id: 4,
-    nombre: "pizza Especial",
-    precio: 1000,
-    ingredientes: ["Muzzarella", "Tomate", "Rucula", "Jamón"],
-    imagen: "./img/especial.png",
-  },
+      const input = document.getElementById('input');
+      const pizzaId = parseInt(input.value);
 
-  {
-    id: 5,
-    nombre: "pizza con Anana",
-    precio: 600,
-    ingredientes: ["Muzzarella", "Tomate", "Anana"],
-    imagen: "./img/anana.png",
-  },
-];
+      if (isNaN(pizzaId)) {
+        showError('Por favor, ingrese un número válido.');
+        return;
+      }
 
-const form = document.getElementById("form");
-const input = document.getElementById("input");
-const resultado = document.getElementById("resultado");
+      const pizza = getPizzaById(pizzaId);
 
-let itemsARenderizar = [];
+      if (pizza) {
+        renderPizzaCard(pizza);
+      } else {
+        showError('No se encontró una pizza con el ID proporcionado.');
+      }
 
-const renderItems = () => {
-  resultado.innerHTML = itemsARenderizar.map((item) => {
-    return `
-    <p> ${item} </p>
-    <img src="${pizzas.imagen}" alt="${pizza.nombre}">
-    <p>Precio: $${pizzas.precio}</p>
-  `;
-  });
-  resultado.innerHTML = "";
-};
+      input.value = '';
+    }
 
-const submitHandle = (e) => {
-  e.preventDefault();
+    function getPizzaById(id) {
+      const pizzas = [
+        {
+          id: 1,
+          nombre: "pizza de Muzzarella",
+          precio: 500,
+          ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+          imagen: "./img/muzzarella.png",
+        },
+      
+        {
+          id: 2,
+          nombre: "pizza de Cebolla",
+          precio: 1500,
+          ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+          imagen: "./img/cebolla.png",
+        },
+      
+        {
+          id: 3,
+          nombre: "pizza 4 Quesos",
+          precio: 1380,
+          ingredientes: [
+            "Muzzarella",
+            "Tomate",
+            "Queso Azul",
+            "Parmesano",
+            "Roquefort",
+          ],
+          imagen: "./img/4quesos.png",
+        },
+      
+        {
+          id: 4,
+          nombre: "pizza Especial",
+          precio: 1000,
+          ingredientes: ["Muzzarella", "Tomate", "Rucula", "Jamón"],
+          imagen: "./img/especial.png",
+        },
+      
+        {
+          id: 5,
+          nombre: "pizza con Anana",
+          precio: 600,
+          ingredientes: ["Muzzarella", "Tomate", "Anana"],
+          imagen: "./img/anana.png",
+        },
+      ];
 
-  let itemNuevo = input.value;
+      return pizzas.find(pizza => pizza.id === id);
+    }
 
-  itemsARenderizar = [...itemsARenderizar, itemNuevo];
+    function renderPizzaCard(pizza) {
+      const card = document.createElement('div');
+      card.className = 'card';
+      card.innerHTML = `
+        <h3>${pizza.nombre}</h3>
+        <img src="${pizza.imagen}" alt="${pizza.name}">
+        <p>Precio: $${pizza.precio}</p>
+      `;
 
-  input.value = "";
+      resultado.innerHTML = '';
+      resultado.appendChild(card);
+    }
 
-  renderItems();
-};
+    function showError(message) {
+      const error = document.createElement('p');
+      error.className = 'error';
+      error.textContent = message;
 
-const init = () => {
-  form.addEventListener("submit", submitHandle);
-};
-
-init();
+      resultado.innerHTML = '';
+      resultado.appendChild(error);
+    }
